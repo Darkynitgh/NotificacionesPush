339d56048b6bd2a6f8e617cb27b0aead9c43cb52
jair.rosete21@gmail.com <jair.rosete21@gmail.com>
2025-07-28 20:39:12 -0600

    recuadro

--- srv/notifica.php
+++ srv/notifica.php
@@ -12,51 +12,48 @@ require_once __DIR__ . "/suscripcionElimina.php";
 use Minishlink\WebPush\WebPush;
 
 const AUTH = [
- "VAPID" => [
-  "subject" => "https://notificacionesphp.gilbertopachec2.repl.co/",
-  "publicKey" => "BMBlr6YznhYMX3NgcWIDRxZXs0sh7tCv7_YCsWcww0ZCv9WGg-tRCXfMEHTiBPCksSqeve1twlbmVAZFv7GSuj0",
-  "privateKey" => "vplfkITvu0cwHqzK9Kj-DYStbCH_9AhGx9LqMyaeI6w"
- ]
+  "VAPID" => [
+    "subject" => "https://notificacionesphp.gilbertopachec2.repl.co/",
+    "publicKey" => "BMBlr6YznhYMX3NgcWIDRxZXs0sh7tCv7_YCsWcww0ZCv9WGg-tRCXfMEHTiBPCksSqeve1twlbmVAZFv7GSuj0",
+    "privateKey" => "vplfkITvu0cwHqzK9Kj-DYStbCH_9AhGx9LqMyaeI6w"
+  ]
 ];
 
 ejecutaServicio(function () {
+  $webPush = new WebPush(AUTH);
 
- $webPush = new WebPush(AUTH);
- $mensaje = "Hola desde el equipo ProRam 👋";
+  // ✅ Lee el mensaje enviado por el cliente
+  $datos = json_decode(file_get_contents("php://input"), true);
+  $mensaje = trim($datos["mensaje"] ?? "") ?: "Hola desde el equipo ProRam 👋";
 
- // Envia el mensaje a todas las suscripciones.
+  $pdo = Bd::pdo();
+  $suscripciones = select(
+    pdo: $pdo,
+    from: SUSCRIPCION,
+    mode: PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,
+    opcional: Suscripcion::class
+  );
 
- $pdo = Bd::pdo();
-
- $suscripciones = select(
-  pdo: $pdo,
-  from: SUSCRIPCION,
-  mode: PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,
-  opcional: Suscripcion::class
- );
-
- foreach ($suscripciones as $suscripcion) {
-  $webPush->queueNotification($suscripcion, $mensaje);
- }
- $reportes = $webPush->flush();
-
- // Genera el reporte de envio a cada suscripcion.
- $reporteDeEnvios = "";
- foreach ($reportes as $reporte) {
-  $endpoint = $reporte->getRequest()->getUri();
-  $htmlEndpoint = htmlentities($endpoint);
-  if ($reporte->isSuccess()) {
-   // Reporte de éxito.
-   $reporteDeEnvios .= "<dt>$htmlEndpoint</dt><dd>Éxito</dd>";
-  } else {
-   if ($reporte->isSubscriptionExpired()) {
-    suscripcionElimina($pdo, $endpoint);
-   }
-   // Reporte de fallo.
-   $explicacion = htmlentities($reporte->getReason());
-   $reporteDeEnvios .= "<dt>$endpoint</dt><dd>Fallo: $explicacion</dd>";
+  foreach ($suscripciones as $suscripcion) {
+    $webPush->queueNotification($suscripcion, $mensaje);
   }
- }
 
- devuelveJson(["reporte" => ["innerHTML" => $reporteDeEnvios]]);
+  $reportes = $webPush->flush();
+
+  $reporteDeEnvios = "";
+  foreach ($reportes as $reporte) {
+    $endpoint = $reporte->getRequest()->getUri();
+    $htmlEndpoint = htmlentities($endpoint);
+    if ($reporte->isSuccess()) {
+      $reporteDeEnvios .= "<dt>$htmlEndpoint</dt><dd>Éxito</dd>";
+    } else {
+      if ($reporte->isSubscriptionExpired()) {
+        suscripcionElimina($pdo, $endpoint);
+      }
+      $explicacion = htmlentities($reporte->getReason());
+      $reporteDeEnvios .= "<dt>$endpoint</dt><dd>Fallo: $explicacion</dd>";
+    }
+  }
+
+  devuelveJson(["reporte" => ["innerHTML" => $reporteDeEnvios]]);
 });
